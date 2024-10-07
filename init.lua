cac64db3f77e136ca81f493c8183807473e972f6
ms <vomircom@gmail.com>
2024-10-07 10:56:35 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local vim = vim
+local Plug = vim.fn["plug#"]
+
+vim.call("plug#begin")
+-- Plug("davidosomething/vim-colors-meh")
+-- Plug("ntk148v/komau.vim")
+Plug("aditya-azad/candle-grey")
+vim.call("plug#end")
+
+-- vim.cmd("colorscheme meh")
+vim.cmd("colorscheme candle-grey")
