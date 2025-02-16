821a92db6f844835d13c189756c4148414ff5db8
ms <vomircom@gmail.com>
2025-02-16 15:46:59 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+# install brew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+# install packages
+brew install \
+  zsh \
+  neovim \
+  terraform \
+  kubectl \
+  fzf \
+  awscli \
+  tsh \
+  node \
+  tmux \
+  bat \
+  go \
+  kubecolor
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,32 @@
+return {
+  "folke/noice.nvim",
+  opts = function()
+    require("noice").setup({
+      cmdline = {
+        -- enabled = true,
+        -- view = "cmdline_popup",
+        format = {
+          cmdline = { pattern = "^:", icon = "", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+          filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+          lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+          input = { view = "cmdline_input", icon = "󰥻 " },
+        },
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 5,
+            col = "50%",
+          },
+          size = {
+            width = auto,
+            height = "auto",
+          },
+        },
+      },
+    })
+  end,
+}
